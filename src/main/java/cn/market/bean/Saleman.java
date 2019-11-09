83a44efa1cbcd13269355d7da95a16e2086c492f
jxy <1796362795@qq.com>
2019-11-09 22:26:06 +0800

    add saleman

---
+++ src/main/java/cn/market/bean/Saleman.java
@@ -0,0 +1,32 @@
+package cn.market.bean;
+
+public class Saleman {
+	private String userid;
+	private String username;
+	private String todaydate;
+	private String counttime;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTodaydate() {
+		return todaydate;
+	}
+	public void setTodaydate(String todaydate) {
+		this.todaydate = todaydate;
+	}
+	public String getCounttime() {
+		return counttime;
+	}
+	public void setCounttime(String counttime) {
+		this.counttime = counttime;
+	}
+}
